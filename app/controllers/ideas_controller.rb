8adbcadc338b7932ea99a7bd2d2440072a8de229
Elton de Andrade Rodrigues <eltonrodrigues61@gmail.com>
2019-05-16 14:20:14 -0300

    basic card insert

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,4 +2,28 @@ class IdeasController < ApplicationController
     def index
         @ideas = Idea.all
     end
+
+    def new
+        @idea = Idea.new
+    end
+
+    def create
+
+        @idea = Idea.new users_params
+        if @idea.save
+            flash[:notice] = "Ideia salva com sucesso"
+            redirect_to root_url
+        else
+            renderiza :new
+        end
+    end
+
+    def update
+        puts "testado o upload "
+    end
+
+    def users_params
+        params.require(:idea).permit(:title,:text)
+    end
+
 end
