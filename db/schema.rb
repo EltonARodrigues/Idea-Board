0b2d3cc6bb5f803e714404322d18f07ff100cb26
Elton de Andrade Rodrigues <eltonrodrigues61@gmail.com>
2019-05-18 13:22:00 -0300

    s3 initial config

--- db/schema.rb
+++ db/schema.rb
@@ -33,14 +33,6 @@ ActiveRecord::Schema.define(version: 2019_05_17_114557) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "feeds", force: :cascade do |t|
-    t.string "name"
-    t.text "path_cover"
-    t.text "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.text "text"
